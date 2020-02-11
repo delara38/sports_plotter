95c25da5df8a098855ced7401b4672f3614c6ceb
delara38 <natan.de.lara@gmail.com>
2020-02-11 13:38:46 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='sports_plotter',
-      version='0.3',
+      version='1.0',
       description='package making it easier to plot on soccer pitches and hockey rinks',
-      download_url = 'https://github.com/delara38/sports_plotter/archive/0.3.tar.gz',
+      download_url = 'https://github.com/delara38/sports_plotter/archive/1.0.tar.gz',
       url='https://github.com/delara38/sports_plotter',
       author='Nathan de Lara',
       author_email='natan.de.lara@gmail.com',
